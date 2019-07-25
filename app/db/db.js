b7cf84b4768506eb186e9cc615e2fb0b18899bd1
vipinpetwal <48955607+vipinpetwal@users.noreply.github.com>
2019-07-25 19:16:12 +0530

    Add files via upload

---
+++ app/db/db.js
@@ -0,0 +1,20 @@
+const users_list=
+    [{
+        id: 1,
+        name: "Richard12",
+        email: "richard@piedpiper.com",   
+        pass:"test"    
+    },
+    {
+        id: 2,
+        name: "Bertram34",
+        email: "gilfoyle@piedpiper.com",
+        pass:"test"
+    }];
+
+module.exports = {    
+    users_list
+}
+
+
+
