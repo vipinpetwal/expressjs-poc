e80f4f5a451cb0cc09b48950eaee77bfa6c28297
vipinpetwal <48955607+vipinpetwal@users.noreply.github.com>
2019-08-01 19:19:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var loginrouter=require('./app/controller/login');
-
+var searchrouter=require('./app/controller/search');
 var globalconfig=require('./config/globalconfig');  //read config file
 
 // defining the Express app
@@ -17,6 +17,7 @@ app.use(cors());
 
 app.post('/api/v1/login',loginrouter.logincheck)
 app.get('/api/v1/displaydata',loginrouter.displaydata)
+app.post('/api/v1/displayfilelist',searchrouter.displayfilelist)
 
 app.listen(globalconfig.port,globalconfig.ip, () => {
     console.log('listening on port 3001');
